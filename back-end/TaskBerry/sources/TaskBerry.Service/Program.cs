f4c6df14a64c52a33e25035dc75ab571fcb755d4
Ali Bozna <alibozna@gmail.com>
2019-09-08 20:34:54 +0200

    Initial back-end commit

---
+++ back-end/TaskBerry/sources/TaskBerry.Service/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskBerry.Service
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore;
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+    }
+}
---
+++ back-end/TaskBerry/sources/TaskBerry.Service/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace TaskBerry.Service
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Configuration;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+
+        public IConfiguration Configuration { get; }
+    }
+}
