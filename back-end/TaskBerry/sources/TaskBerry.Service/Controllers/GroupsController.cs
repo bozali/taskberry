7c47c4f95f7c5a7b69f2a7e796d24ea98b523ba8
Ali Bozna <alibozna@gmail.com>
2019-09-11 16:46:44 +0200

    Adding Swashbuckle for REST documentation

--- back-end/TaskBerry/sources/TaskBerry.Service/Controllers/GroupsController.cs
+++ back-end/TaskBerry/sources/TaskBerry.Service/Controllers/GroupsController.cs
@@ -1,5 +1,11 @@
 ﻿namespace TaskBerry.Service.Controllers
 {
+    using Swashbuckle.AspNetCore.Annotations;
+
+    using TaskBerry.Shared.Entities;
+
+    using System.Collections.Generic;
+
     using Microsoft.AspNetCore.Mvc;
 
 
@@ -9,6 +15,13 @@
     {
         public GroupsController()
         {
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Gets groups.", Description = "Gets a null.")]
+        public ActionResult<IEnumerable<GroupEntity>> Get()
+        {
+            return null;
         }
     }
 }
--- back-end/TaskBerry/sources/TaskBerry.Service/Startup.cs
+++ back-end/TaskBerry/sources/TaskBerry.Service/Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace TaskBerry.Service
+﻿using Swashbuckle.AspNetCore.Swagger;
+
+namespace TaskBerry.Service
 {
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Configuration;
@@ -18,6 +20,11 @@
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(options =>
+            {
+                options.EnableAnnotations();
+                options.SwaggerDoc("v1", new Info { Title = "TaskBerry", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -26,6 +33,14 @@
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskBerry");
+                options.RoutePrefix = "docs";
+            });
 
             app.UseMvc();
         }
