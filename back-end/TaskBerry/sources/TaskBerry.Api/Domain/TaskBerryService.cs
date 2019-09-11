6080f6b083f5c89415abacbfbe458dc5702e3df3
Ali Bozna <alibozna@gmail.com>
2019-09-11 19:57:14 +0200

    Edit TaskBerryService in Client API.

--- back-end/TaskBerry/sources/TaskBerry.Api/Domain/TaskBerryService.cs
+++ back-end/TaskBerry/sources/TaskBerry.Api/Domain/TaskBerryService.cs
@@ -1,7 +1,19 @@
-﻿namespace TaskBerry.Api.Domain
+﻿using TaskBerry.Api.Interfaces;
+
+namespace TaskBerry.Api.Domain
 {
-    public class TaskBerryService
+    public class TaskBerryService : ITaskBerryService
     {
-        
+        private readonly TaskBerryContext _context;
+
+        public TaskBerryService(TaskBerryContext context)
+        {
+            this._context = context;
+        }
+
+        public IGroupService CreateGroupService()
+        {
+            return new GroupService(this._context);
+        }
     }
 }
