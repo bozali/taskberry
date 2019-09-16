c893dddb6b1700b053b448994c44e85563a95aaa
Ali Bozna <alibozna@gmail.com>
2019-09-16 21:24:04 +0200

    Add TasksController and template functions

--- back-end/TaskBerry/sources/TaskBerry.Service/Controllers/TasksController.cs
+++ back-end/TaskBerry/sources/TaskBerry.Service/Controllers/TasksController.cs
@@ -1,17 +1,41 @@
 ﻿namespace TaskBerry.Service.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
 
     /// <summary>
     /// </summary>
     [ApiController]
-    public class TasksController
+    public class TasksController : ControllerBase
     {
         /// <summary>
         /// </summary>
         public TasksController()
         {
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpPost]
+        [Produces("application/json")]
+        public IActionResult MoveTask()
+        {
+            return this.Ok();
+        }
+
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpPost]
+        [Produces("application/json")]
+        public IActionResult AssignTaskToUser()
+        {
+            return this.Ok();
         }
     }
 }
