fd7924b6e8a9a64ee93eb5b518b8a5501fac3466
Ali Bozna <alibozna@gmail.com>
2019-09-24 21:55:54 +0200

    Configure Kestrel and prepared for deploying

---
+++ back-end/TaskBerry/sources/TaskBerry.Service/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskBerry.Service.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+
+    [ApiController]
+    [Route("/api")]
+    public class ApiController
+    {
+        [HttpGet("/api/version")]
+        [Produces("application/json")]
+        public ActionResult<string> GetVersion()
+        {
+            return "TaskBerry v1";
+        }
+    }
+}
