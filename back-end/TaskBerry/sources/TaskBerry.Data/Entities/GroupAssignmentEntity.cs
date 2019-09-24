df4bba391ef9ff16a9460b80667e07c0811ebd07
Ali Bozna <alibozna@gmail.com>
2019-09-25 00:15:11 +0200

    Added Sandcastle base documentation

--- back-end/TaskBerry/sources/TaskBerry.Data/Entities/GroupAssignmentEntity.cs
+++ back-end/TaskBerry/sources/TaskBerry.Data/Entities/GroupAssignmentEntity.cs
@@ -5,18 +5,25 @@
     using System;
 
 
+    /// <summary>
+    /// Represents a database entity for the group assignments.
+    /// </summary>
     [Table("GroupAssignment")]
     public class GroupAssignmentEntity
     {
+        /// <inheritdoc cref="Models.Group"/>
         [Key]
         public Guid Id { get; set; }
 
+        /// <inheritdoc cref="Models.Group"/>
         public int UserId { get; set; }
 
+        /// <inheritdoc cref="Models.Group"/>
         [MaxLength(36)]
         [ForeignKey("Group")]
         public Guid GroupId { get; set; }
 
+        /// <inheritdoc cref="Models.Group"/>
         public GroupEntity Group { get; set; }
     }
 }
